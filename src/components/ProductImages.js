66c0d4747bf2685a2b0f25d5f2f86857cb126120
JothikaAnbarasu <jo.frozen64@gmail.com>
2023-04-20 23:33:17 +0530

    updated featuredProd in homePg,updated singleProductpg

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const ProductImages = () => {
-  return <h4>product images</h4>
+const ProductImages = ({ images = [{ url: '' }] }) => {
+  const [main, setMain] = useState(images[0])
+  return <Wrapper>
+    <img src={main.url}
+      alt="main image"
+      className='main' />
+    <div className="gallery">
+      {images.map((image, index) => {
+        return (
+          <img
+            src={image.url}
+            alt={image.filename}
+            key={index}
+            onClick={() => setMain(images[index])}
+            className={`${image.url === main.url ? 'active' : null}`} />
+        )
+      })}
+
+    </div>
+
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
@@ -18,7 +37,7 @@ const Wrapper = styled.section`
   .gallery {
     margin-top: 1rem;
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(5, 2fr);
     column-gap: 1rem;
     img {
       height: 100px;
